bf3f340ad9b4c67c94202704f677e5d7291ce15b
KottenAlin <156342596+KottenAlin@users.noreply.github.com>
2026-01-23 22:22:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h3>test</h3>
+  <div>
+    just nu finns det inget fint på hemsidan med det är enkelt att fixa
+  </div>
+</template>
+
+<style scoped></style>
